fdbef0da9a8e1b32df794744912ce2059d66be74
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-06-04 10:48:12 +0530

    frontend home page created

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Home } from '@components'
+
+export default function App() {
+  return (<>
+  <Home/>
+  </>
+  )
+}
